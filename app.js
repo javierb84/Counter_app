56f594eade0bb66a576f044d3f4b1fea8da74712
Javier Briones <javierbblancas@hotmail.com>
2026-01-06 17:29:21 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//document.getElementById("count-el").innerHTML = 5;
+
+// intialize the count as 0
+let count = 0;
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let saves = count + " - ";
+    saveEl.textContent += saves;
+    count = 0;
+    countEl.textContent = count;
+}
